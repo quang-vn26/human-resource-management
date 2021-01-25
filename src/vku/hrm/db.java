af8f4e7296c6d10f95e2513cd193043d91436b2c
Quangg <65973279+quang-vn26@users.noreply.github.com>
2021-01-25 16:47:14 +0700

    'first'

---
+++ src/vku/hrm/db.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vku.hrm;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author Admin
+ */
+public class db {
+    Connection conn = null;
+    public static Connection java_db(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String url = "jdbc:sqlite:C:\\Users\\Admin\\Desktop\\QuanLyNhanSu\\QuanLyNhanSu\\data.sqlite";
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
